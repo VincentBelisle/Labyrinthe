2cda8837241f4b1d94312f9e45113e308cf21c1c
felix <michaud07@outlook.com>
2023-05-08 09:33:17 -0400

    ajout de la mort

--- Labyrinthe/Assets/CherryController.cs
+++ Labyrinthe/Assets/CherryController.cs
@@ -23,10 +23,4 @@ public class CherryController : MonoBehaviour
             Destroy(gameObject, 1f);
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
